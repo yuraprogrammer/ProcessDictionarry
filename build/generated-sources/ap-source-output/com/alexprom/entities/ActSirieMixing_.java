1512520c227362929f01ccb51e3dcbeb5c86b107
yuraprogrammer <yuraprogrammer1979@gmail.com>
2018-06-11 21:25:20 +0300

    Добавлена новая сущность смеси сырьевой смеси. Добавлены данные о сырьевой смеси в акт.

---
+++ build/generated-sources/ap-source-output/com/alexprom/entities/ActSirieMixing_.java
@@ -0,0 +1,18 @@
+package com.alexprom.entities;
+
+import java.math.BigDecimal;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-11T21:15:00")
+@StaticMetamodel(ActSirieMixing.class)
+public class ActSirieMixing_ { 
+
+    public static volatile SingularAttribute<ActSirieMixing, BigDecimal> sirieVolume;
+    public static volatile SingularAttribute<ActSirieMixing, Long> actID;
+    public static volatile SingularAttribute<ActSirieMixing, Long> id;
+    public static volatile SingularAttribute<ActSirieMixing, BigDecimal> sirieDensity;
+    public static volatile SingularAttribute<ActSirieMixing, BigDecimal> sirieMass;
+
+}
