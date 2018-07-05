bbe6411d698ea1ef0a8d4ccd14350ba42f9444c0
yuraprogrammer <yuraprogrammer1979@gmail.com>
2018-07-05 16:52:54 +0300

    Добавлены данные о температуре на выходе печи

---
+++ build/generated-sources/ap-source-output/com/alexprom/entities/process/VFurnaceOutTemp_.java
@@ -0,0 +1,19 @@
+package com.alexprom.entities.process;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-05T09:14:48")
+@StaticMetamodel(VFurnaceOutTemp.class)
+public class VFurnaceOutTemp_ { 
+
+    public static volatile SingularAttribute<VFurnaceOutTemp, String> aTag;
+    public static volatile SingularAttribute<VFurnaceOutTemp, BigDecimal> maxValue;
+    public static volatile SingularAttribute<VFurnaceOutTemp, Date> aDate;
+    public static volatile SingularAttribute<VFurnaceOutTemp, Integer> aShift;
+    public static volatile SingularAttribute<VFurnaceOutTemp, Long> id;
+
+}
